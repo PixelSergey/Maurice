045d221abb14b8d6c8c3575c7d831ea17ad590cb
PixelSergey <sergey.ichtchenko@gmail.com>
2017-06-10 13:40:23 +0300

    Improve Maurice a lot Add: Ping/Pong command and text response Flip command; flips the previous sent line of text Shutdown command: Disconnects the bot from the server Change: using the "bot" and not "client" class for everything Other minor changes

--- maurice.py
+++ maurice.py
@@ -1,27 +1,65 @@
 import discord
+from discord.ext import commands
 import asyncio
+import sys
+import os
 
-client = discord.Client()
+bot = commands.Bot(command_prefix="m!", description="A very interesting bot")
 
-@client.event
+
+@bot.event
 @asyncio.coroutine
-def on_message(message):  
-    if message.author == client.user:
+def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print(bot.user.id)
+    print("------")
+
+
+@bot.command()
+@asyncio.coroutine
+def ping():
+    yield from bot.say("Pong!")
+
+
+@bot.command(pass_context=True)
+@asyncio.coroutine
+def flip(ctx):
+    msg = yield from bot.logs_from(ctx.message.channel, limit=2)
+    msg = list(msg)[1].content[::-1]
+    real = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890<>\"'()[]{}?¿.,!¡&"
+    flipped = "∀qƆpƎℲפHIſʞ˥WNOԀQɹS┴∩ΛMX⅄ZɐqɔpǝɟƃɥᴉɾʞlɯuodbɹsʇnʌʍxʎzƖᄅƐㄣϛ9ㄥ860><„,)(][}{¿?˙'¡!⅋"
+    tbl = str.maketrans(real, flipped)
+    msg = msg.translate(tbl)
+    yield from bot.say(msg)
+
+
+@bot.command()
+@asyncio.coroutine
+def shutdown():
+    yield from bot.say("Shutting down Maurice...")
+    yield from bot.say("Bye")
+    yield from bot.logout()
+
+
+@bot.event
+@asyncio.coroutine
+def on_message(message):
+    if message.author == bot.user:  # Do not respond to self
         return
     if message.content.startswith("hello") or message.content.startswith("hi"):
         msg = "Hello {0.author.mention}".format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from bot.send_message(message.channel, msg)
+    yield from bot.process_commands(message)
+    if message.content.startswith("ping"):
+        yield from bot.send_message(message.channel, "Pong!")
 
-@client.event
-@asyncio.coroutine
-def on_ready():  
-    print("Logged in as")
-    print(client.user.name)
-    print(client.user.id)
-    print("------")
 
+# Get key, initialize bot
+os.chdir(sys.path[0])
 secret = open("secret.key", "r")
 key = secret.readline().strip()
 secret.close()
 
-client.run(key)
+bot.run(key)
+
